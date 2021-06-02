122fc05694596bf1ca5d9f2ec08a4e074b77b81f
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-06-02 17:38:49 +0100

    CORRECTIONS
    Corrected syntax such as : instead of = and ensured a ; was closing the list

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  {name:"Bulbasaur", height:2.04, type:["grass", "poison"]},
+  {name:"Venusaur", height:6.07, type:["seed", "overgrown"]},
+{name:"Charmander", height:2.00, type:["fire"]}
+];
