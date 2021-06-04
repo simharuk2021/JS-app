faa50c7d88ad23466ee544dd6dab6a2a960f9329
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-06-04 14:32:41 +0100

    CHANGE TO TYPE
    type name changed to types in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-  {name:"Bulbasaur", height:2.04, type:["grass", "poison"]},
-  {name:"Venusaur", height:6.07, type:["seed", "overgrown"]},
-{name:"Charmander", height:2.00, type:["fire"]}
+  {name:"Bulbasaur", height:2.04, types:["grass", "poison"]},
+  {name:"Venusaur", height:6.07, types:["seed", "overgrown"]},
+{name:"Charmander", height:2.00, types:["fire"]}
 ];
