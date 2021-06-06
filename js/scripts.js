ef305bc09288026a458fa7cac4417ee05b6a5e34
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-06-06 15:26:21 +0100

    STLYE CHANGES
    normalised CSS
    Added logo image to the header
    Added class to allow CSS targeting
    Added img class to increase size of logo
    imported font for the header text
    changed position of display to ensure presentation is centrally aligned
    Added types to the output
    Added comments to the JS code
    Added viewport meta tag

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ let pokemonList = [
   {name:"Venusaur", height:6.07, types:["seed", "overgrown"]},
 {name:"Charmander", height:2.00, types:["fire"]}
 ];
+// This code searches for the Pokemon's within the Array and loops through them all.
+for (let i = 0; i < pokemonList.length; i++) {
+  //The if statement looks for the pokemon which has a height over 6.00
+  if (pokemonList[i].height>=6.00) {
+    /* The document.write statement then lists the Pokemon name and height whilst providing some spacing
+    between text fields and the arrays being returned  */
+  document.write (pokemonList[i].name + ' (height: ' +pokemonList[i].height +') (type: ' +pokemonList[i].types +') -The biggest Pokemon! ' + '<br>');
+  //The statement returns the information in the arrays for all the other pokemon, regardless of their height//
+} else{document.write (pokemonList[i].name + ' (height: ' +pokemonList[i].height +') (type: ' +pokemonList[i].types +') ' + '<br>');}
+}
