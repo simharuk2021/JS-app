aee133b70efed231b2426437258cfa5a352d5653
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-06-11 15:36:26 +0100

    ADD FUNCTION, LARGEST REINSTATEMENT AND DOCUMENT WRITE ADDED
    Added the ability to add a pokemon to the repository
    
    reinstated the height over 7 statement
    
    added a new pokemon -Lapras to test functionality of all functions
    
    changed console.log to document.write and changes added to  spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-/*The below code creates a repository within a function which is already executed. Within the Repository is the array of Pokemon*/
-
 let pokemonRepository = (function () {
   let pokemonList = [{name:"Bulbasaur", height:2.04, types:["grass", "poison"]},
   {name:"Venusaur", height:6.07, types:["seed", "overgrown"]},
@@ -21,5 +19,14 @@ let pokemonRepository = (function () {
     getAll: getAll
   };
 })();
-pokemonRepository.add({name:'Metapod'});
-console.log(pokemonRepository.getAll());
+pokemonRepository.add({name:'Lapras', height:"8.02"});
+
+//listing all the pokemons and their hight
+pokemonRepository.getAll().forEach(function(item) {
+  //if the pokemon is higher then 7 than write: This is the biggest pokemon
+  if (item.height > 7) {
+    document.write(item.name + ' is ' + item.height +' feet high!' + ' - This is the biggest pokemon!' + '<br>');
+  } else {
+    document.write(item.name + ' is ' + item.height +' feet high!' + '<br>');
+  }
+})
