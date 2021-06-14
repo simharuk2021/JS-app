759998a6bb1425825d190eb88774e2309b3c4400
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-06-14 15:07:59 +0100

    COMMENTING
    Correction to spelling and grammar of comments

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,13 @@ let pokemonRepository = (function () {
     getAll: getAll
   };
 })();
+//This code adds a new pokemon to the list with the details listed as below
 pokemonRepository.add({name:'Lapras', height:"8.02"});
 
-//listing all the pokemons and their hight
+//listing all the pokemons and their height
 pokemonRepository.getAll().forEach(function(item) {
   //if the pokemon is higher then 7 than write: This is the biggest pokemon
-  if (item.height > 7) {
+  if (item.height > 5) {
     document.write(item.name + ' is ' + item.height +' feet high!' + ' - This is the biggest pokemon!' + '<br>');
   } else {
     document.write(item.name + ' is ' + item.height +' feet high!' + '<br>');
