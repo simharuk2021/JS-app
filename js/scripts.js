d7982e7b3013d6ae80f0d592b2405b251b027885
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-06-23 15:01:43 +0100

    EVENT LISTENER AND SHOW DETAILS ADDED
    click event listener added to the buttons
    function which shows the clicked button details in the console log

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,13 @@ function addListItem (pokemon){
   button.classList.add("button-class");
   listpokemon.appendChild(button);
   pokemonList.appendChild(listpokemon);
+  button.addEventListener('click', function()
+  {showDetails(pokemon)
+  });
+}
+
+function showDetails(pokemon){
+console.log(pokemon);
 }
 
 /* the below code returns the add and getAll functions */
